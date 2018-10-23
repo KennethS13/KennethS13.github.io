1ad92e37a1b5509170976e4792679ebca4a93f20
kenneths <snipe1331@gmail.com>
2018-10-22 23:30:44 -0600

    Second Commit

--- Word-Guess-Game/assets/javascript/game.js
+++ Word-Guess-Game/assets/javascript/game.js
@@ -1,6 +1,6 @@
 var keyUp;
 var keysArray = [];
-var lettersGuessed;
+var lettersGuessed = "";
 var displayedGuesses = [];
 var lettersDisplayed = [];
 var savedLetters = [];
@@ -12,6 +12,7 @@ var pokemonLettersArray = [];
 var mysteryLetters = "";
 var mysteryLettersArray = [];
 var remainingGuesses = 20;
+var guessesDisplayed = document.getElementById("remaining-guesses").textContent = remainingGuesses;
 var playerWins = 0;
 var playerLosses = 0;
 var match;
@@ -31,11 +32,12 @@ var randomPokemon = pokemonArray[Math.floor(Math.random() * pokemonArray.length)
 var pokemonLettersArray = randomPokemon.split("");
 // This loop uses the split characters and changes them into an underscore, and then writes them to the html
 for (i = 0; i < pokemonLettersArray.length; i++) {
-    mysteryLetters += " _ ";
+	mysteryLettersArray.push("_");
+	mysteryLetters = mysteryLettersArray.join(" ")
 }
 document.getElementById("random-pokemon").textContent = mysteryLetters.toUpperCase();
 console.log(mysteryLetters)
-
+console.log(randomPokemon);
 
 document.onkeyup = function(q) {
 
@@ -51,92 +53,115 @@ document.onkeyup = function(q) {
 //If keyUp is equal to the value in the keysArray, then assign currentLetter to the one letter strings (alphabet).
 function keyCodeToChar(){
 	if (keyUp == keysArray[0]){
-		currentLetter = "A";
+		currentLetter = "a";
 	}
 	else if (keyUp == keysArray[1]){
-		currentLetter = "B";
+		currentLetter = "b";
 	}
 	else if (keyUp == keysArray[2]){
-		currentLetter = "C";
+		currentLetter = "c";
 	}
 	else if (keyUp == keysArray[3]){
-		currentLetter = "D";
+		currentLetter = "d";
 	}
 	else if (keyUp == keysArray[4]){
-		currentLetter = "E";
+		currentLetter = "e";
 	}
 	else if (keyUp == keysArray[5]){
-		currentLetter = "F";
+		currentLetter = "f";
 	}
 	else if (keyUp == keysArray[6]){
-		currentLetter = "G";
+		currentLetter = "g";
 	}
 	else if (keyUp == keysArray[7]){
-		currentLetter = "H";
+		currentLetter = "h";
 	}
 	else if (keyUp == keysArray[8]){
-		currentLetter = "I";
+		currentLetter = "i";
 	}
 	else if (keyUp == keysArray[9]){
-		currentLetter = "J";
+		currentLetter = "j";
 	}
 	else if (keyUp == keysArray[10]){
-		currentLetter = "K";
+		currentLetter = "k";
 	}
 	else if (keyUp == keysArray[11]){
-		currentLetter = "L";
+		currentLetter = "l";
 	}
 	else if (keyUp == keysArray[12]){
-		currentLetter = "M";
+		currentLetter = "m";
 	}
 	else if (keyUp == keysArray[13]){
-		currentLetter = "N";
+		currentLetter = "n";
 	}
 	else if (keyUp == keysArray[14]){
-		currentLetter = "O";
+		currentLetter = "o";
 	}
 	else if (keyUp == keysArray[15]){
-		currentLetter = "P";
+		currentLetter = "p";
 	}
 	else if (keyUp == keysArray[16]){
-		currentLetter = "Q";
+		currentLetter = "q";
 	}
 	else if (keyUp == keysArray[17]){
-		currentLetter = "R";
+		currentLetter = "r";
 	}
 	else if (keyUp == keysArray[18]){
-		currentLetter = "S";
+		currentLetter = "s";
 	}
 	else if (keyUp == keysArray[19]){
-		currentLetter = "T";
+		currentLetter = "t";
 	}
 	else if (keyUp == keysArray[20]){
-		currentLetter = "U";
+		currentLetter = "u";
 	}
 	else if (keyUp == keysArray[21]){
-		currentLetter = "V";
+		currentLetter = "v";
 	}
 	else if (keyUp == keysArray[22]){
-		currentLetter = "W";
+		currentLetter = "w";
 	}
 	else if (keyUp == keysArray[23]){
-		currentLetter = "X";
+		currentLetter = "x";
 	}
 	else if (keyUp == keysArray[24]){
-		currentLetter = "Y";
+		currentLetter = "y";
 	}
 	else if (keyUp == keysArray[25]){
-		currentLetter = "Z";
+		currentLetter = "z";
 	}
 	else if (keyUp == 32){
 		currentLetter = "space";
-    }
-    document.getElementById("guessed-letters").textContent = currentLetter
-    console.log(currentLetter)
-}
-
-function addGuessedLetters() {
-	if (keyUp === currentLetter)
-	currentLetter = [guessedLetters]
-	console.log(guessedLetters)
+	}
+    
+	console.log(currentLetter);
+	
+	
+	if (pokemonLettersArray.indexOf(currentLetter) === -1) {
+		if (guessedLetters.indexOf(currentLetter) !== -1) {
+			return;
+		}
+		guessesDisplayed = guessesDisplayed--;
+		console.log(guessesDisplayed);
+		guessedLetters.push(currentLetter);
+		lettersGuessed = guessedLetters.join(" ")
+		document.getElementById("guessed-letters").textContent = lettersGuessed.toUpperCase();
+		console.log(guessedLetters);
+	}
+	else {
+		if (guessedLetters.indexOf(currentLetter) !== -1) {
+			return;
+		}
+		for (j = 0; j < pokemonLettersArray.length; j++) {
+			if (pokemonLettersArray[j] === currentLetter) {
+				mysteryLettersArray[j] = currentLetter;
+				mysteryLetters = mysteryLettersArray.join(" ")
+				document.getElementById("random-pokemon").textContent = mysteryLetters.toUpperCase();
+			}
+		}
+		guessedLetters.push(currentLetter);
+		lettersGuessed = guessedLetters.join(" ")
+		document.getElementById("guessed-letters").textContent = lettersGuessed.toUpperCase();	
+	}
+	console.log(mysteryLettersArray);
 }

a0f7612d76b08280f3fe0453e6854135f5132c22
kenneths <snipe1331@gmail.com>
2018-10-26 12:56:52 -0600

    First Commit

---
+++ Unit-4-game/assets/javascript/game.js
@@ -0,0 +1,12 @@
+// Golbal variables
+//
+// variable for random number to be displayed to player
+var targetRandomNumber = "";
+//
+// variable to store player wins
+var wins = 0;
+//
+// variable to store player losses
+var losses = 0;
+//
+//
